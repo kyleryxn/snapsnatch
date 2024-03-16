27dccce313591610bdf99880843bfba32a3299e6
Kyle <kyle.schoenhardt@mail.citytech.cuny.edu>
2024-03-16 15:39:38 -0400

    [feat] Add new `DefaultImageFactory` class for creating unknown image types

---
+++ src/main/java/com/github/kyleryxn/snapsnatch/image/factory/DefaultImageFactory.java
@@ -0,0 +1,30 @@
+package com.github.kyleryxn.snapsnatch.image.factory;
+
+import com.github.kyleryxn.snapsnatch.image.model.DefaultImage;
+import com.github.kyleryxn.snapsnatch.image.model.Image;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Component;
+
+public class DefaultImageFactory implements ImageFactory {
+    private final ImageFactoryUtil imageFactoryUtil;
+    private Element imageTag;
+
+    public DefaultImageFactory(ImageFactoryUtil imageFactoryUtil) {
+        this.imageFactoryUtil = imageFactoryUtil;
+    }
+
+    @Override
+    public void setImageTag(Element imageTag) {
+        this.imageTag = imageTag;
+    }
+
+    @Override
+    public Image createImage() {
+        String description = imageFactoryUtil.parseDescription(imageTag);
+        boolean isLogo = imageFactoryUtil.parseLogo(imageTag);
+        String url = imageFactoryUtil.parseURL(imageTag);
+
+        return new DefaultImage(description, isLogo, url);
+    }
+
+}
