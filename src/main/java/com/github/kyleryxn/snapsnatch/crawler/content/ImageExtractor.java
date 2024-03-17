5a73cb4c7cb3b3eef3e79537be95fc2ca5b468b8
Kyle <kyle.schoenhardt@mail.citytech.cuny.edu>
2024-03-16 21:55:25 -0400

    [refactor] Refactor ImageExtractor class to use ImageService
    This commit updates the `ImageExtractor` class in the `com.github.kyleryxn.snapsnatch.crawler.content` package, streamlining its implementation by using the `ImageService`. The previous factory-based logic for image creation has been replaced with a more cohesive and centralized approach through `ImageService`, simplifying the image extraction process. This change enhances maintainability and decouples the image extraction logic from specific image types, making the system more flexible and easier to extend in the future.

--- src/main/java/com/github/kyleryxn/snapsnatch/crawler/content/ImageExtractor.java
+++ src/main/java/com/github/kyleryxn/snapsnatch/crawler/content/ImageExtractor.java
@@ -1,62 +1,31 @@
 package com.github.kyleryxn.snapsnatch.crawler.content;
 
-import com.github.kyleryxn.snapsnatch.image.factory.*;
+import com.github.kyleryxn.snapsnatch.image.ImageService;
 import com.github.kyleryxn.snapsnatch.image.model.Image;
-import com.github.kyleryxn.snapsnatch.crawler.util.ImageURLProcessor;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 @Component
-public class ImageExtractor implements ElementExtractor {
-    private final Map<String, Supplier<ImageFactory>> factories;
-    private final ImageURLProcessor processor;
+class ImageExtractor implements ElementExtractor {
+    private final ImageService imageService;
 
-    public ImageExtractor() {
-        this.factories = new HashMap<>();
-        this.processor = new ImageURLProcessor();
-
-        factories.put("gif", GIFImageFactory::new);
-        factories.put("jpeg", JPGImageFactory::new);
-        factories.put("png", PNGImageFactory::new);
-        factories.put("svg", SVGImageFactory::new);
+    public ImageExtractor(ImageService imageService) {
+        this.imageService = imageService;
     }
 
     @Override
-    public Set<Image> extractImages(Elements elements) {
-        Set<Image> images;
-
-        images = elements.stream()
-                .map(tag -> {
-                    String url = tag.attr("src");
-                    processor.setURL(url);
-                    String extension = processor.getImageExtension();
-
-                    return processTag(extension, tag);
-                })
-                .filter(Objects::nonNull)
-                .collect(Collectors.toSet());
-
-        return images;
+    public String getExtractorType() {
+        return getClass().getSimpleName();
     }
 
-    private Image processTag(String extension, Element tag) {
-        Supplier<ImageFactory> factorySupplier = factories.get(extension);
-
-        if (factorySupplier != null) {
-            ImageFactory factory = factorySupplier.get();
-            factory.setImageTag(tag);
-            return factory.createImage();
-        } else {
-            return null;
-        }
+    @Override
+    public Set<Image> extractImages(Elements imageTags) {
+        return imageTags.stream()
+                .map(imageService::processImage)
+                .collect(Collectors.toSet());
     }
 
 }
