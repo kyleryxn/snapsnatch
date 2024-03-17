ed91ccc0f16b2a0b04215b3180c8724e1bf3c76e
Kyle <kyle.schoenhardt@mail.citytech.cuny.edu>
2024-03-17 12:14:20 -0400

    [test] Add tests for HTMLParser

---
+++ src/test/java/com/github/kyleryxn/snapsnatch/crawler/content/HTMLParserTest.java
@@ -0,0 +1,17 @@
+package com.github.kyleryxn.snapsnatch.crawler.content;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DisplayName("HTMLParser Tests")
+class HTMLParserTest {
+
+    @Autowired
+    private HTMLParser htmlParser;
+
+
+}
