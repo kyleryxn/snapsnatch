e2a5e25f57bc36db6563db6c60b8479d02da1718
Kyle <kyle.schoenhardt@mail.citytech.cuny.edu>
2024-06-05 21:05:31 -0400

    [feat] Add DataSourceConfig class for future database connection
    Added the `DataSourceConfig` class to configure the database connection using HikariCP. This class includes properties for the database URL, username, password, and driver class name, as well as settings for connection pooling. This setup ensures efficient and reliable database connectivity for future use.

---
+++ src/main/java/com/github/kyleryxn/snapsnatch/common/DataSourceConfig.java
@@ -0,0 +1,43 @@
+package com.github.kyleryxn.snapsnatch.common;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class DataSourceConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Bean
+    public DataSource dataConfig() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setMinimumIdle(2);
+        dataSource.setIdleTimeout(30000L);
+        dataSource.setConnectionTimeout(20000L);
+        dataSource.setMaxLifetime(1800000L);
+
+        return dataSource;
+    }
+
+}
